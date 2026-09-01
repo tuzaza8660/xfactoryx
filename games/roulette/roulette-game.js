f2853169cd851b6c8fb0254fe0c13ccaed59f814
khl9880 <khl9880@gmail.com>
2026-09-02 08:43:26 +0900

    Use wood and metal roulette room themes

--- games/roulette/roulette-game.js
+++ games/roulette/roulette-game.js
@@ -3,7 +3,7 @@ import { GAME_IDS, getCurrentRound, getGameHistory, getWallet, placeBet } from '
 import { watchRoomPresence } from '../../js/services/room-service.js?v=portal-rooms-1';
 import { RoulettePhysics, RED_NUMBERS } from './prototype/roulette-physics.js';
 import { RoulettePlayer } from './prototype/roulette-player.js';
-import { RouletteRenderer } from './prototype/roulette-renderer.js?v=themes-1';
+import { RouletteRenderer } from './prototype/roulette-renderer.js?v=wood-themes-1';
 
 const $ = id => document.getElementById(id);
 const physics = new RoulettePhysics();
